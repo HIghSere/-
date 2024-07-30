b695cd35b7ead71e9a840ef23468d117d971bc58
melatnix <144571368+HIghSere@users.noreply.github.com>
2024-07-30 09:22:18 +0900

    Update ua-gen.js

--- dist/util/ua-gen.js
+++ dist/util/ua-gen.js
@@ -6,6 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.UAGen = UAGen;
 const fs_1 = __importDefault(require("fs"));
 function UAGen() {
-    const userAgents = fs_1.default.readFileSync("./data/user-agents.txt", "utf-8");
+    const userAgents = fs_1.default.readFileSync("../data/user-agents.txt", "utf-8");
     return userAgents[Math.floor(Math.random() * userAgents.length)];
 }
